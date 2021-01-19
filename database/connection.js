e7e00b04d1e96c8872879981892136f46dde4084
Jose077 <64561246+Jose077@users.noreply.github.com>
2021-01-19 15:38:41 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('localiza', 'root', 'sandeson321', {
+const connection = new Sequelize('localiza', '******', '********', {
     host: 'localhost',
     dialect: 'mysql'
 });
